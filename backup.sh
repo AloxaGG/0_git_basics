5176f165d83ba90ac485d7195848f5dda8a1b5cd
Aleksey Nikitin <st02200286@gse.cs.msu.ru>
2022-10-02 14:20:43 +0300

    New comment Done

---
+++ backup.sh
@@ -0,0 +1,9 @@
+echo "Hello World"
+echo "The current directory is:"
+pwd
+echo "The user logged in is:"
+whoami
+echo "Home for the current user is: $HOME"
+money=62000
+name="Bitcoin"
+echo "$name is trading $money dollars now"
